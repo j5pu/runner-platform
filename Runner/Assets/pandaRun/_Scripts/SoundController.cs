a04c5726ee9d4c1564b683256a7ae6cc351dc0e2
Michel Barbou Salvador <mibarbou@gmail.com>
2015-02-09 11:58:36 +0100

    Subida Inicial del Template del juego con su .gitignore

---
+++ Runner/Assets/pandaRun/_Scripts/SoundController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public AudioClip  clickSound,singleJumpSound;
+	public AudioClip coinHitSound;
+	public AudioClip StarsSound; 
+	public AudioClip coinCounting; 
+	public static SoundController Static ;
+	public AudioSource[]  audioSources;
+	void Start () {
+		Static = this;
+	}
+	
+	// Update is called once per frame
+
+	//for single Jump
+	public void PlaySingleJumpSound()
+	{
+		
+		swithAudioSources(singleJumpSound);
+	}
+	 
+	//---------------------------------
+
+	public void PlayClickSound()
+	{
+		 
+		swithAudioSources(clickSound);
+	}
+	 
+
+	public void playcoinCounting()
+	{
+		
+		audio.PlayOneShot(coinCounting);
+	}
+
+		public void playCoinHit()
+	{
+		
+		audio.PlayOneShot(coinHitSound);
+	}
+
+	public void playStarsSound()
+	{
+		
+		audio.PlayOneShot(StarsSound);
+	}
+	
+
+	 
+	 
+
+
+	void swithAudioSources( AudioClip clip)
+	{
+		if(audioSources[0].isPlaying) 
+		{
+			audioSources[1].PlayOneShot(clip);
+		}
+		else audioSources[0].PlayOneShot(clip);
+
+	}
+}
